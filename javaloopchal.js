9074496b57f2681b467557ce5bd04103b159e065
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-04-26 14:09:58 -0700

    commit1

---
+++ javaloopchal.js
@@ -0,0 +1,16 @@
+for(let i=1; i<=20; i++){
+    if(0 == i % 2){
+        console.log(i-1)
+    }
+}
+let array = [4, 2.5, 1, -0.5, -2, -3.5];
+for (let a of array) {
+    console.log(a);
+}
+var sum=0;
+for(let i=1;i<=100;i++){
+    
+    console.log(sum, "+", i,"+")
+    sum=sum+i;
+}
+var num = 1
